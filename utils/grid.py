f3a44a2de47793db744d5413ff0ac2d7c816c8ae
phegan <freaks051082>
2016-03-04 17:36:38 -0500

    Updated the algorthm to deal with the grid being shitty, instead of actually working

--- utils/grid.py
+++ utils/grid.py
@@ -1,11 +1,40 @@
 
 
 def calculate_distance(start, destination):
-    start_x = start / 100
-    start_y = start % 10
-    destination_x = destination / 100
-    destination_y = destination % 10
 
-    distance = (abs(start_x - destination_x) + abs(start_y - destination_y)) - 1
+    start_coords = parse_coords(start)
+    dest_coords = parse_coords(destination)
 
-    print distance
+    # outer_diff = (abs(start_x - destination_x) + abs(start_y - destination_y))
+    #
+    # inner_diff = abs(abs(start_x + start_y) - abs(destination_x + destination_y)) / 2
+
+    # dist_calc = lambda sx, sy, dx, dy: (abs(sx - dx) + abs(sy - dy))
+    #
+    # inner_diff = (abs(sx - sy) - abs(dx - dy))
+    #
+    # distance = dist_calc(start_x, start_y, destination_x, destination_y)
+
+
+    print outer_diff
+    print inner_diff
+
+
+def traverse_hex(start, destination):
+    start_coords = parse_coords(start)
+    dest_coords = parse_coords(destination)
+
+
+    #first coord even or odd
+    odd_connections = [(1,0), (-1,0), (0,1), (0,-1), (-1,1),(1,1)]
+    even_connections = [(1,0), (-1,0), (0,1), (0,-1), (1,-1),(-1,-1)]
+
+
+def parse_coords(coords):
+    return [coords / 100, coords % 10]
+
+def main():
+    calculate_distance(101, 202)
+
+if __name__ == "__main__":
+    main()
