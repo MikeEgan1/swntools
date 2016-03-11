84c8a8abcf2de83d874a45a3a34a3716b44e64a7
Mike <phegan@gmail.com>
2016-03-11 11:26:14 -0500

    Updated the breadth first search to be complete, and then added dijkstras search. Changed map over the HexMap, and created a abstract map class.

---
+++ utils/pathfinding/pathing.py
@@ -0,0 +1,10 @@
+class Pathing(object):
+    def __init__(self, grid, graph, queue):
+        self.grid = grid
+
+    """
+    @start
+    @end
+    :return
+    """
+    def plot_path(self, start, end):
---
+++ utils/travel/travel.py
@@ -0,0 +1,9 @@
+class Travel(object):
+    def __init__(self, distance, start, end, cost, path):
+        self.distance = distance
+        self.start = start
+        self.end = end
+        self.cost = cost
+        self.path = path
+        
+
